dadb6f9d290afcc93f806202f7ce466d55c8e333
Hannes <hannes@fam-schuetz.de>
2021-10-24 22:21:16 +0200

    update basic layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex flex-col items-stretch w-full min-h-screen  border-l-3 border-r-3 sm:border-t-3"
+    class="flex flex-col items-stretch w-full min-h-screen  border-l-3 border-r-3"
   >
     <!-- Mobile Logo -->
     <NuxtLink
@@ -26,29 +26,30 @@
       </div>
     </NuxtLink>
 
-    <div class="sm:flex">
-      <!-- Desktop Logo -->
-      <NuxtLink
-        to="/"
-        class="sticky top-0 z-40 justify-center flex-shrink-0 hidden h-screen overflow-hidden  w-14 sm:block border-r-3 lg:w-18"
-      >
-        <DesktopLogo
-          class="
-            absolute
-            top-0
-            w-10
-            lg:w-14
-            left-[7px]
-            lg:left-[8px]
-            animate-logoScroll
-          "
-        />
-      </NuxtLink>
+    <!-- Desktop Border top -->
+    <div class="sticky top-0 hidden sm:block border-t-3"></div>
 
-      <!-- Content -->
-      <div class="flex-grow overflow-x-hidden">
-        <Nuxt />
-      </div>
+    <!-- Desktop Logo -->
+    <NuxtLink
+      to="/"
+      class="fixed top-0 left-0 z-40 justify-center flex-shrink-0 hidden h-full overflow-hidden  w-14 sm:block border-r-3 lg:w-18"
+    >
+      <DesktopLogo
+        class="
+          absolute
+          top-0
+          w-10
+          lg:w-14
+          left-[7px]
+          lg:left-[8px]
+          animate-logoScroll
+        "
+      />
+    </NuxtLink>
+
+    <!-- Content -->
+    <div class="flex-grow overflow-x-hidden sm:pl-[53px] lg:pl-[69px]">
+      <Nuxt />
     </div>
 
     <!-- Footer -->
