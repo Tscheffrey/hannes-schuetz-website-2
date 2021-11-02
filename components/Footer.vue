8d830487443653a198ce31c68bbfee7532809b7b
Hannes <hannes@fam-schuetz.de>
2021-11-02 21:32:51 +0100

    add comments

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,64 +1,19 @@
 <template>
   <div
-    class="
-      box-content
-      sticky
-      bottom-0
-      z-40
-      flex flex-shrink-0
-      h-12
-      border-t-3
-      bg-secondary-100
-      border-b-3
-    "
+    class="box-content sticky bottom-0 z-40 flex flex-shrink-0 h-12 border-t-3 bg-secondary-100 border-b-3"
   >
     <!-- Theme switcher -->
     <button
-      class="
-        box-content
-        relative
-        flex-shrink-0
-        w-12
-        overflow-hidden
-        border-r-3
-        sm:border-r-0 sm:border-l-3 sm:order-2
-      "
+      class="box-content relative flex-shrink-0 w-12 overflow-hidden border-r-3 sm:border-r-0 sm:border-l-3 sm:order-2"
       @click="toggleDarkMode"
     >
       <div
-        class="
-          absolute
-          top-0
-          left-0
-          flex
-          items-center
-          justify-center
-          w-full
-          h-full
-          transition-transform
-          duration-300
-          dark:translate-x-12 dark:-translate-y-12 dark:rotate-180
-        "
+        class="absolute top-0 left-0 flex items-center justify-center w-full h-full transition-transform duration-300 dark:translate-x-12 dark:-translate-y-12 dark:rotate-180"
       >
         <SunIcon />
       </div>
       <div
-        class="
-          absolute
-          top-0
-          left-0
-          flex
-          items-center
-          justify-center
-          w-full
-          h-full
-          transition-transform
-          duration-300
-          -rotate-180
-          -translate-x-12
-          translate-y-12
-          dark:translate-x-0 dark:translate-y-0 dark:rotate-0
-        "
+        class="absolute top-0 left-0 flex items-center justify-center w-full h-full transition-transform duration-300 -rotate-180 -translate-x-12 translate-y-12 dark:translate-x-0 dark:translate-y-0 dark:rotate-0"
       >
         <MoonIcon />
       </div>
@@ -66,43 +21,16 @@
 
     <!-- Language switcher -->
     <div
-      class="
-        box-content
-        relative
-        flex-shrink-0
-        w-12
-        overflow-hidden
-        font-semibold
-        border-r-3
-        sm:border-r-0 sm:border-l-3 sm:order-3
-        group
-        user-select-none
-      "
+      class="box-content relative flex-shrink-0 w-12 overflow-hidden font-semibold border-r-3 sm:border-r-0 sm:border-l-3 sm:order-3 group user-select-none"
     >
       <NuxtLink
-        class="
-          w-full
-          h-full
-          uppercase
-          transition-opacity
-          duration-300
-          hover-hover:opacity-0 hover-hover:group-hover:opacity-100
-        "
+        class="w-full h-full uppercase transition-opacity duration-300 hover-hover:opacity-0 hover-hover:group-hover:opacity-100"
         :to="switchLocalePath($i18n.locale === 'de' ? 'en' : 'de')"
       >
         <transition name="language-switcher-1">
           <div
             v-if="$i18n.locale === 'en'"
-            class="
-              absolute
-              top-0
-              left-0
-              flex
-              items-center
-              justify-center
-              w-full
-              h-full
-            "
+            class="absolute top-0 left-0 flex items-center justify-center w-full h-full "
           >
             en
           </div>
@@ -111,16 +39,7 @@
         <transition name="language-switcher-2">
           <div
             v-if="$i18n.locale === 'de'"
-            class="
-              absolute
-              top-0
-              left-0
-              flex
-              items-center
-              justify-center
-              w-full
-              h-full
-            "
+            class="absolute top-0 left-0 flex items-center justify-center w-full h-full "
           >
             de
           </div>
@@ -128,39 +47,15 @@
       </NuxtLink>
 
       <div
-        class="
-          absolute
-          top-0
-          left-0
-          flex
-          items-center
-          justify-center
-          w-full
-          h-full
-          transition-opacity
-          duration-300
-          opacity-0
-          pointer-events-none
-          hover-hover:opacity-100 hover-hover:group-hover:opacity-0
-        "
+        class="absolute top-0 left-0 flex items-center justify-center w-full h-full transition-opacity duration-300 opacity-0 pointer-events-none hover-hover:opacity-100 hover-hover:group-hover:opacity-0"
       >
         <GlobeIcon />
       </div>
     </div>
 
+    <!-- Footer Links -->
     <ul
-      class="
-        flex
-        items-center
-        w-full
-        h-full
-        px-5
-        overflow-x-auto
-        font-semibold
-        uppercase
-        sm:order-1 sm:px-3
-        lg:px-4
-      "
+      class="flex items-center w-full h-full px-5 overflow-x-auto font-semibold uppercase sm:order-1 sm:px-3 lg:px-4"
     >
       <li class="mr-4 lg:mr-6 last:mr-0">
         <NuxtLink :to="localePath('/imprint')">
